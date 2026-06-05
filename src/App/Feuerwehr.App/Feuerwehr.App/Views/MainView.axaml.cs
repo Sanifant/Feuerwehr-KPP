74bf2e12dde61153f86b10dd258a18e86b5d9b3f
Stefan Petersen <stefan@openelp.de>
2026-06-05 20:27:05 +0200

    removed webassembly

---
+++ src/App/Feuerwehr.App/Feuerwehr.App/Views/MainView.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Feuerwehr.App.Views
+{
+    public partial class MainView : UserControl
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
